931598e67e5cd834be24e647b9e669e545c78899
Rahul Patte <rahulthemeshopy@gmail.com>
2024-10-17 19:59:50 +0530

    Added dotenv package.

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -8,6 +8,8 @@ import {
 import { MongoDBSessionStorage } from '@shopify/shopify-app-session-storage-mongodb';
 import { restResources } from "@shopify/shopify-api/rest/admin/2024-07";
 import prisma from "./db.server";
+import dotenv from "dotenv"
+dotenv.config();
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
